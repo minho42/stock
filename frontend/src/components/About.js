6a333e3d52e3b4cbd4d2d42977b774bbf8295b0d
Min ho Kim <minho42@gmail.com>
2023-01-30 01:04:26 +1100

    remove unused

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -5,17 +5,6 @@ export const About = () => {
         <div className="flex flex-col justify-center max-w-md w-full text-2xl"></div>
       </div>
 
-      {/* <p>
-        <a
-          className="underline"
-          href="https://hellostake.com/au/referral?referrer=minhok522"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Invest in US Stocks and ETFs on Stake
-        </a>
-      </p> */}
-
       <div className="flex justify-center text-center space-x-3">
         <a
           className="link-black"
